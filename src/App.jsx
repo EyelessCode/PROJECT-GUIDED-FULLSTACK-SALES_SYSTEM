734260ff2d336d095bcebc426f029d2d323032db
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-09-04 00:46:42 -0500

    Add global styles and breakpoints, update App layout
    Introduces GlobalStyles and Breakpoints for consistent styling and responsive design. Updates App.jsx to use styled-components for layout and applies global styles. Adds index.js for central exports. Updates dependencies to include styled-components and icon libraries.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import styled from "styled-components"
+import {GlobalStyle} from "./index"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Container>
+      <GlobalStyle/>
+      <section className="contentSidebar"></section>
+      <section className="contentMenuambur"></section>
+      <section className="contentRouters"></section>
+    </Container>
   )
 }
+
+const Container=styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: gray;
+`;
 
 export default App
---
+++ src/styles/Breakpoints.js
@@ -0,0 +1,13 @@
+const sizes={
+    mobile:"576px",
+    tablet:"768px",
+    laptop:"992",
+    desktop:"1200px"
+}
+
+export const Device={
+    mobile:`(min-width:${sizes.mobile})`,
+    tablet:`(min-width:${sizes.tablet})`,
+    laptop:`(min-width:${sizes.laptop})`,
+    desktop:`(min-width:${sizes.desktop})`,
+}
